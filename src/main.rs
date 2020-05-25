7cbff53cb20d0568397616e2e46355b8c1606cee
tkhr0 <tkhr.karasawa@gmail.com>
2020-05-26 00:44:02 +0900

    add command that it regists a new profile

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,74 @@
 extern crate base32;
+extern crate clap;
 extern crate mfa_cli;
 
+use clap::{App, Arg, SubCommand};
+use mfa_cli::config;
 use mfa_cli::totp;
+use std::process;
 
 fn main() {
+    let config_file_path = match config::initialize() {
+        Ok(path) => path,
+        Err(err) => panic!(err), // TODO: safe exit
+    };
+    let config = match config::Config::restore(&config_file_path) {
+        Ok(config) => config,
+        Err(_) => panic!("failed to restore config"), // TODO: safe exit
+    };
+
+    let args = build_option_parser().get_matches();
+
+    match args.subcommand() {
+        ("profile", Some(profile_args)) => match profile_args.subcommand() {
+            ("add", Some(add_args)) => {
+                let profile = config::Profile::new(
+                    add_args.value_of("account_name").unwrap(),
+                    add_args.value_of("key").unwrap(),
+                );
+                let mut config = config;
+                config.push_profile(profile);
+                if let Err(_) = config.dump(&config_file_path) {
+                    panic!("failed to dump config");
+                };
+                println!("added new profile");
+                process::exit(0);
+            }
+            _ => {}
+        },
+        _ => {}
+    }
+
     let raw_secret = "AAAAAAA";
 
+    // TODO: 設定ファイルが空の時
     let secret = base32::decode(base32::Alphabet::RFC4648 { padding: true }, raw_secret).unwrap();
     let code = totp::totp(&secret);
 
     println!("{:?}", code.unwrap());
+}
+
+// オプション
+fn build_option_parser<'a, 'b>() -> App<'a, 'b> {
+    App::new("awskey").subcommand(
+        SubCommand::with_name("profile")
+            .about("profile settings")
+            .subcommand(
+                SubCommand::with_name("add")
+                    .about("Add a new profile")
+                    .help("Add a new profile")
+                    .arg(
+                        Arg::with_name("account_name")
+                            .takes_value(true)
+                            .required(true)
+                            .help("account name"),
+                    )
+                    .arg(
+                        Arg::with_name("key")
+                            .takes_value(true)
+                            .required(true)
+                            .help("secret"),
+                    ),
+            ),
+    )
 }
