6fdb0d1473a73091e72c48b9fa56817d8420aaa1
tkhr0 <tkhr.karasawa@gmail.com>
2020-05-30 11:11:17 +0900

    refactor totp code

--- src/totp.rs
+++ src/totp.rs
@@ -5,23 +5,22 @@ use std::time::SystemTime;
 // Time step
 const TIME_STEP: u64 = 30;
 
-// TOTP を計算する
+// TOTP を現在時刻から計算する
 pub fn totp(secret: &[u8]) -> Result<String, String> {
-    let current_time = match current_time() {
-        Ok(time) => time,
+    match current_time() {
+        Ok(current_time) => gen_totp(secret, current_time),
         Err(err) => return Err(err),
-    };
+    }
+}
 
-    let t = current_time / TIME_STEP;
+// TOTP を任意の時刻で計算する
+fn gen_totp(secret: &[u8], time: u64) -> Result<String, String> {
+    let t = time / TIME_STEP;
 
     let mut byte_t = Vec::new();
     byte_t.write_u64::<BigEndian>(t).unwrap();
 
-    gen_totp(secret, &byte_t)
-}
-
-fn gen_totp(secret: &[u8], counter: &[u8]) -> Result<String, String> {
-    hotp::hotp(secret, counter)
+    hotp::hotp(secret, &byte_t)
 }
 
 // UNIX time からの経過秒数を返す
