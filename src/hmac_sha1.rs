256f494cc392ddd5dacdeff3fa8407f71992b5b5
tkhr0 <tkhr.karasawa@gmail.com>
2020-06-08 00:02:48 +0900

    replace sha-1 crate

--- src/hmac_sha1.rs
+++ src/hmac_sha1.rs
@@ -3,50 +3,13 @@ extern crate generic_array;
 extern crate hmac;
 extern crate sha1;
 
-use digest::generic_array::typenum::{U20, U64};
+use digest::generic_array::typenum::U20;
 use digest::generic_array::GenericArray;
 use hmac::{Hmac, Mac};
 use sha1::Sha1;
 
 type OutputSize = U20;
-type HmacSha1 = Hmac<Sha1Engine>;
-
-#[derive(Clone)]
-struct Sha1Engine {
-    engine: Sha1,
-}
-
-impl digest::Input for Sha1Engine {
-    fn input<B: AsRef<[u8]>>(&mut self, data: B) {
-        self.engine.update(data.as_ref());
-    }
-}
-
-impl digest::BlockInput for Sha1Engine {
-    type BlockSize = U64;
-}
-
-impl digest::FixedOutput for Sha1Engine {
-    type OutputSize = U20;
-
-    fn fixed_result(self) -> GenericArray<u8, Self::OutputSize> {
-        GenericArray::from(self.engine.digest().bytes())
-    }
-}
-
-impl digest::Reset for Sha1Engine {
-    fn reset(&mut self) {
-        self.engine = Sha1::new();
-    }
-}
-
-impl Default for Sha1Engine {
-    fn default() -> Self {
-        Self {
-            engine: Sha1::new(),
-        }
-    }
-}
+type HmacSha1 = Hmac<Sha1>;
 
 // HMAC-SHA-1 を計算する
 pub fn gen_hmac_sha1(key: &[u8], input: &[u8]) -> Result<GenericArray<u8, OutputSize>, String> {
